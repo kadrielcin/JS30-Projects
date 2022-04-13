cb31d4644d96f82bd5fffb3deac2c04aba4df482
kadri <kadrielcin@yahoo.com>
2022-04-13 22:21:55 +0200

    cizgifilm

--- Projects/Project-11/scripts.js
+++ Projects/Project-11/scripts.js
@@ -7,10 +7,39 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
+// Build out Functions
 function togglePlay() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  const method = video.paused ? "play" : "pause";
+  video[method]();
 }
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  console.log(icon);
+  toggle.textContent = icon;
+}
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.name);
+  console.log(this.value);
+}
+function handleUpdate() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = 
+}
+// Hook up the event listeners
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
