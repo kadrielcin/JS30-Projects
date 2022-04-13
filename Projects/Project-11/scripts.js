4f8348ff3ac6ac2d82e31af1fba0b48df2ba3b1b
kadri <kadrielcin@yahoo.com>
2022-04-13 23:17:28 +0200

    cizi

--- Projects/Project-11/scripts.js
+++ Projects/Project-11/scripts.js
@@ -27,15 +27,21 @@ function handleRangeUpdate() {
   console.log(this.name);
   console.log(this.value);
 }
-function handleUpdate() {
+function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = 
+  progressBar.style.flexBasis = `${percent}%`;
 }
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 // Hook up the event listeners
 
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
@@ -43,3 +49,8 @@ ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
 );
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mousedown", () => (mousedown = false));
