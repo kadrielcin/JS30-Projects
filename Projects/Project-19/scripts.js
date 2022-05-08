a46be354573f0c063f43ae3f175fab1dda8e9607
kadri <kadrielcin@yahoo.com>
2022-05-08 18:40:56 +0200

    8

--- Projects/Project-19/scripts.js
+++ Projects/Project-19/scripts.js
@@ -30,7 +30,10 @@ function paintToCanvas() {
     // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
     // mess with them
-    pixels = redEffect(pixels);
+    // pixels = redeffect(pixels)
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+    pixels = greenScreen(pixels);
     // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -62,11 +65,40 @@ function redEffect(pixels) {
 function rgbsplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i - 150] = pixels.data[i + 0]; //red
-    pixels.data[i + 100] = pixels.data[i + 1]; //green
-    pixels.data[i + 150] = pixels.data[i + 2]; //blue
+    pixels.data[i + 500] = pixels.data[i + 1]; //green
+    pixels.data[i - 550] = pixels.data[i + 2]; //blue
   }
   return pixels;
 }
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
 getVideo();
 
 video.addEventListener("canplay", paintToCanvas);
