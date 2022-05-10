6f2afe51e5858b932f6d2009293426920e1cf105
kadri <kadrielcin@yahoo.com>
2022-05-10 09:01:58 +0200

    1

--- Projects/Project-19/scripts.js
+++ Projects/Project-19/scripts.js
@@ -10,6 +10,13 @@ function getVideo() {
     .then((localMediaStream) => {
       console.log(localMediaStream);
 
+      //  DEPRECIATION :
+      //       The following has been depreceated by major browsers as of Chrome and Firefox.
+      //       video.src = window.URL.createObjectURL(localMediaStream);
+      //       Please refer to these:
+      //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+      //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+
       video.srcObject = localMediaStream;
       video.play();
     })
@@ -21,7 +28,6 @@ function getVideo() {
 function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
-  console.log(width, height);
   canvas.width = width;
   canvas.height = height;
 
@@ -30,46 +36,49 @@ function paintToCanvas() {
     // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
     // mess with them
-    // pixels = redeffect(pixels)
-    // pixels = rgbSplit(pixels);
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
     // ctx.globalAlpha = 0.8;
-    pixels = greenScreen(pixels);
+
+    // pixels = greenScreen(pixels);
     // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
 function takePhoto() {
-  //playeed the sound
+  // played the sound
   snap.currentTime = 0;
   snap.play();
 
-  //take the data out of the canvas
+  // take the data out of the canvas
   const data = canvas.toDataURL("image/jpeg");
   const link = document.createElement("a");
   link.href = data;
   link.setAttribute("download", "handsome");
-  link.innerHtml = `<img src="${data}" alt="Handsome Man" />`;
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; //red
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
 }
 
-function rgbsplit(pixels) {
+function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; //red
-    pixels.data[i + 500] = pixels.data[i + 1]; //green
-    pixels.data[i - 550] = pixels.data[i + 2]; //blue
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
   }
   return pixels;
 }
+
 function greenScreen(pixels) {
   const levels = {};
 
