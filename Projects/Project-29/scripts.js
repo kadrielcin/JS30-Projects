2b6525dace98eb5bd13bb4d424384f117c94339b
kadri <kadrielcin@yahoo.com>
2022-06-27 10:21:42 +0200

    Prject 29 second check

---
+++ Projects/Project-29/scripts.js
@@ -0,0 +1,16 @@
+let countdown;
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check of we should stop it!
+    if (secondsLeft <= 0) {
+      return;
+    }
+    // display it
+    console.log(secondsLeft);
+  }, 1000);
+}
